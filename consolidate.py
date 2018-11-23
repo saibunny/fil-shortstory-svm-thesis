645fa343c27424e3564d7c62dfa79ce04f165763
Kendrick Tan <kenjay227@gmail.com>
2018-11-23 15:06:52 +0800

    Update consolidate.py

--- consolidate.py
+++ consolidate.py
@@ -5,24 +5,24 @@ def loadDataset(filename):
         lines = csv.reader(csvfile)
         return list(lines)
 
-
-
-
-
 counti = 0
 for i in range(6):
     consolidated = []
     counti = i + 1
 
-    for j in range(37):
+    for j in range(4):
         countj = j+1
-        inputdirectory = "data\\validset_batch" + str(countj) + "_processed_wordcount" + str(counti)
+        # inputdirectory = "data\\validset_batch" + str(countj) + "_processed_wordcount" + str(counti)
+        # inputdirectory = "data\\testsetpred_batch" + str(countj) + "_processed_wordcount" + str(counti)
+        inputdirectory = "data\\validset_withnegation_batch" + str(countj) + "_processed_wordcount" + str(counti)
+
         dataset = loadDataset(inputdirectory + ".csv")
 
         for row in dataset:
             consolidated.append(row)
 
-        outputdirectory = "data\\consolidated_validset_processed_wordcount" + str(counti) + ".csv"
+        # outputdirectory = "data\\consolidated_validset_processed_wordcount" + str(counti) + ".csv"
+        outputdirectory = "data\\consolidated_validset_withnegation_processed_wordcount" + str(counti) + ".csv"
         with open(outputdirectory, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(consolidated)
